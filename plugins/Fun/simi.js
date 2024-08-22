68434575cfdbaa9e49c6ca9075d6adfcbf705539
LeonBot909 <phmgaming909@gmail.com>
2024-08-23 05:19:03 +0700

    Add files via upload

--- plugins/Fun/simi.js
+++ plugins/Fun/simi.js
@@ -1,28 +1,40 @@
-async function sendMessageToSimSimi(message) {
-    const url = 'https://simsimi.vn/web/simtalk';
-    
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ text: message })  // Ganti dengan struktur data yang benar jika diperlukan
-        });
+          import axios from "axios";
+          let handler = async (m, { conn, text }) => {
+            const chat = global.db.data.chats[m.chat];
 
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
-        const data = await response.json();
-        return data.response;  // Ganti 'response' dengan key yang benar sesuai data yang diterima
-    } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
-    }
-}
-handler.help = ["simi"];
+            if (text == "off") {
+              chat.simi = false;
+              m.reply("Berhasil mematikan auto simi di group ini");
+            } else if (text == "on") {
+              chat.simi = true;
+              m.reply("Berhasil mengaktifkan auto simi di group ini");
+            } else {
+              if (!text) throw "Mau ngomong apa kak sama simi?";
+              try {
+                let simi = await getMessage(text, "id");
+                m.reply(simi);
+              } catch (e) {
+                throw "Maaf kak aku ga paham hehehe...";
+              }
+            }
+          }; 
+          handler.help = ["simi"];
           handler.tags = ["fun"];
           handler.command = /^(simi)$/i;
           handler.onlyprem = true;
           handler.limit = true;
           export default handler;
+
+          async function getMessage(yourMessage, langCode) {
+            const res = await axios.post(
+              "https://api.simsimi.vn/v2/simtalk",
+              new URLSearchParams({
+                text: yourMessage,
+                lc: langCode,
+              })
+            );
+
+            if (res.status > 200) throw new Error(res.data.success);
+
+            return res.data.message;
+          }
